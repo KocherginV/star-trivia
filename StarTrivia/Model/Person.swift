9785eeb040eeef139237ca02dcf47cab827eb240
KocherginV <volodymyrkochergin@gmail.com>
2019-06-17 17:54:55 +0200

    add json parsing

---
+++ StarTrivia/Model/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by Volodymyr Kochergin on 17.06.19.
+//  Copyright © 2019 Volodymyr Kochergin. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let height: String
+    let mass: String
+    let hair: String
+    let birthYear: String
+    let gender: String
+    let homeworldUrl: String
+    let filmUrls: [String]
+    let vehicleUrls: [String]
+    let starshipUrls: [String]
+}
--- StarTrivia/Services/PersonApi.swift
+++ StarTrivia/Services/PersonApi.swift
@@ -26,17 +26,28 @@ class PersonApi {
             do {
                 let jsonAny = try JSONSerialization.jsonObject(with: data, options: [])
                 guard let json = jsonAny as? [String: Any] else { return }
-                print(json)
+                self.parsePersonManual(json: json)
             } catch {
                 debugPrint(error.localizedDescription)
                 return
             }
-            
-            print("Data = \(data)")
-            print("Response = \(response)")
-            print("Error = \(error)")
         }
         
         task.resume()
+    }
+    
+    private func parsePersonManual(json: [String: Any]) -> Person{
+        let name = json["name"] as? String ?? ""
+        let height = json["height"] as? String ?? ""
+        let mass = json["mass"] as? String ?? ""
+        let hair = json["hair_color"] as? String ?? ""
+        let birthYear = json["birth_year"] as? String ?? ""
+        let gender = json["gender"] as? String ?? ""
+        let homeWorldUrl = json["homeworld"] as? String ?? ""
+        let filmUrls = json["films"] as? [String] ?? [String]()
+        let vehicleUrls = json["vehicles"] as? [String] ?? [String]()
+        let starshipUrls = json["starships"] as? [String] ?? [String]()
+        
+        return Person (name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrl: homeWorldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
     }
 }
