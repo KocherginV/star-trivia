1fd64919e95a05e8acf86972a6be5b6bc8f8872e
KocherginV <volodymyrkochergin@gmail.com>
2019-06-25 09:41:29 +0200

    re-implement json parsing using swiftyjson

--- StarTrivia/Services/PersonApi.swift
+++ StarTrivia/Services/PersonApi.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class PersonApi {
     
-    //Web request with Alamofire:
+    // Web request with AF and SwiftyJson
     
     func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
         
@@ -23,15 +24,39 @@ class PersonApi {
                 return
             }
             
-            guard let json = response.result.value as? [String: Any] else {
-                return completion(nil)
+            guard let data = response.data else { return completion(nil) }
+            do {
+                let json = try JSON(data: data)
+                let person = self.parsePersonSwifty(json: json)
+                completion(person)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
             }
-            let person = self.parsePersonManual(json: json)
-            completion(person)
         }
-        
     }
     
+    //Web request with Alamofire:
+    
+//    func getRandomPersonAlamo(id: Int, completion: @escaping PersonResponseCompletion) {
+//
+//        guard let url = URL(string: "\(PERSON_URL)\(id)") else { return }
+//        Alamofire.request(url).responseJSON { (response) in
+//            if let error = response.result.error {
+//                debugPrint(error.localizedDescription)
+//                completion(nil)
+//                return
+//            }
+//
+//            guard let json = response.result.value as? [String: Any] else {
+//                return completion(nil)
+//            }
+//            let person = self.parsePersonManual(json: json)
+//            completion(person)
+//        }
+//
+//    }
+//
     //Web request with URL Session:
     
     func getRandomPersonSession(id: Int, completion: @escaping PersonResponseCompletion) {
@@ -64,6 +89,23 @@ class PersonApi {
         task.resume()
     }
     
+    // parsing with SwiftyJson
+    private func parsePersonSwifty(json: JSON) -> Person{
+        let name = json["name"].stringValue
+        let height = json["height"].stringValue
+        let mass = json["mass"].stringValue
+        let hair = json["hair_color"].stringValue
+        let birthYear = json["birth_year"].stringValue
+        let gender = json["gender"].stringValue
+        let homeWorldUrl = json["homeworld"].stringValue
+        let filmUrls = json["films"].arrayValue.map({$0.stringValue})
+        let vehicleUrls = json["vehicles"].arrayValue.map({$0.stringValue})
+        let starshipUrls = json["starships"].arrayValue.map({$0.stringValue})
+        
+        return Person (name: name, height: height, mass: mass, hair: hair, birthYear: birthYear, gender: gender, homeworldUrl: homeWorldUrl, filmUrls: filmUrls, vehicleUrls: vehicleUrls, starshipUrls: starshipUrls)
+    }
+    
+    // parsing function using manual methods
     private func parsePersonManual(json: [String: Any]) -> Person{
         let name = json["name"] as? String ?? ""
         let height = json["height"] as? String ?? ""
