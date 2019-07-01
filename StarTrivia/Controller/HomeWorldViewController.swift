7a00090b7bcf3a0ce373b8d6b55b90d6820227de
KocherginV <volodymyrkochergin@gmail.com>
2019-07-01 16:57:28 +0200

    add homeworld ui and api

--- StarTrivia/Controller/HomeWorldViewController.swift
+++ StarTrivia/Controller/HomeWorldViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class HomeWorldViewController: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var climateLbl: UILabel!
+    @IBOutlet weak var terrainLbl: UILabel!
+    @IBOutlet weak var populationLbl: UILabel!
+    
     var person: Person!
-
+    let api = HomeworldApi()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(person.name)
+        api.getHomeworl(url: person.homeworldUrl) { (homeworld) in
+            if let homeworld = homeworld {
+                self.setupUi(homeworld: homeworld)
+            }
+        }
     }
-
+    
+    func setupUi(homeworld: Homeworld) {
+        nameLbl.text = homeworld.name
+        climateLbl.text = homeworld.climate
+        terrainLbl.text = homeworld.terrain
+        populationLbl.text = homeworld.population
+    }
+    
 }
---
+++ StarTrivia/Model/Homeworld.swift
@@ -0,0 +1,16 @@
+//
+//  Homeworld.swift
+//  StarTrivia
+//
+//  Created by Volodymyr Kochergin on 01.07.19.
+//  Copyright © 2019 Volodymyr Kochergin. All rights reserved.
+//
+
+import Foundation
+
+struct Homeworld: Codable {
+    let name: String
+    let climate: String
+    let terrain: String
+    let population: String
+}
