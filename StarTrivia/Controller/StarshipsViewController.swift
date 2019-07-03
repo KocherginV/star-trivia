7dd811b67e8548a7fe532d6dede88e4ad23f511f
KocherginV <volodymyrkochergin@gmail.com>
2019-07-03 16:19:40 +0200

    add starships ui

--- StarTrivia/Controller/StarshipsViewController.swift
+++ StarTrivia/Controller/StarshipsViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class StarshipsViewController: UIViewController, PersonProtocol {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var manufacturerLbl: UILabel!
+    @IBOutlet weak var costLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var crewLbl: UILabel!
+    @IBOutlet weak var passengersLbl: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     var person: Person!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func previousClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+    }
+    
 }
