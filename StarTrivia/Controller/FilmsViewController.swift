fee9f2feb21f227708587452d228d71ae230b619
KocherginV <volodymyrkochergin@gmail.com>
2019-07-04 17:50:26 +0200

    add films ui

--- StarTrivia/Controller/FilmsViewController.swift
+++ StarTrivia/Controller/FilmsViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class FilmsViewController: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var episodeLbl: UILabel!
+    @IBOutlet weak var directorLbl: UILabel!
+    @IBOutlet weak var producerLbl: UILabel!
+    @IBOutlet weak var releasedLbl: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var crawlLbl: UITextView!
+    
     var person: Person!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    @IBAction func previousClicked(_ sender: Any) {
+        setButtonState()
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+        setButtonState()
+    }
+    
+    func setButtonState() {
+        
     }
 }
